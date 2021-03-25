f9fadfece2e1b58db45effdfaea839617008267c
oisinmcl <oisin_mcl_@hotmail.com>
2021-03-25 14:44:15 +0000

    Added Mobx for State Management

--- web-ui/src/app/layout/NavBar.tsx
+++ web-ui/src/app/layout/NavBar.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { Button, Container, Menu } from 'semantic-ui-react';
+import { useStore } from '../stores/store';
 
-interface Props {
-    openForm: () => void;
-}
 
-export default function Navbar({ openForm }: Props) {
+export default function Navbar() {
+
+    const { activityStore } = useStore();
+
     return (
         <Menu inverted fixed="top">
             <Container>
@@ -15,7 +15,7 @@ export default function Navbar({ openForm }: Props) {
                 </Menu.Item>
                 <Menu.Item name="Acitivties" />
                 <Menu.Item>
-                    <Button onClick={openForm} positive content='Create'></Button>
+                    <Button onClick={() => activityStore.openForm()} positive content='Create'></Button>
                 </Menu.Item>
             </Container>
         </Menu>
--- web-ui/src/features/activities/dashboard/ActivityList.tsx
+++ web-ui/src/features/activities/dashboard/ActivityList.tsx
@@ -1,16 +1,12 @@
 import React, { SyntheticEvent, useState } from 'react';
-import { Activity } from '../../../app/models/activity';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
+import { useStore } from '../../../app/stores/store';
+import { observer } from 'mobx-react-lite';
 
+export default observer(function ActivityList() {
 
-interface Props {
-    activities: Activity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
-
-export default function ActivityList({ activities, selectActivity, deleteActivity, submitting }: Props) {
+    const { activityStore } = useStore();
+    const { deleteActivity, ActivtiesByDate, loading } = activityStore;
 
     const [target, setTarget] = useState('');
 
@@ -22,7 +18,7 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
     return (
         <Segment >
             <Item.Group divided>
-                {activities.map(activity => (
+                {ActivtiesByDate.map(activity => (
                     <Item key={activity.id} >
                         <Item.Content>
                             <Item.Header as='a'>{activity.title}</Item.Header>
@@ -32,10 +28,10 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
                                 <div>{activity.city}, {activity.venue} </div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue' />
+                                <Button onClick={() => activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue' />
                                 <Button
                                     name={activity.id}
-                                    loading={submitting && target === activity.id}
+                                    loading={loading && target === activity.id}
                                     onClick={(e) => handleActivityDelete(e, activity.id)}
                                     floated='right'
                                     content='Delete'
@@ -49,4 +45,4 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
             </Item.Group>
         </Segment>
     )
-}
+})
